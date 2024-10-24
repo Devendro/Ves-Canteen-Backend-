15540dbdb7d5ccad946b9150c6f14bec57b6f49c
Devendro <devendromaurya521@gmail.com>
2024-10-25 01:54:45 +0530

    updated

---
+++ models/Order.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+const mongoose_delete = require("mongoose-delete");
+const OrderSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      index: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    order: [
+      {
+        orderId: {
+          type: String,
+          index: true
+        },
+        food: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "food",
+        },
+        count: {
+          type: Number,
+          default: 1
+        },
+        preparationNote: {
+          type: String
+        },
+        orderStatus: {
+          type: String,
+          default: "Received"
+        },
+        orderCompleted: {
+          type: Boolean,
+          default: false,
+        },
+        orderCanceled: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    amount: {
+      type: Number,
+    },
+    orderStatus: {
+      type: String,
+    },
+    orderCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    orderCanceled: {
+      type: Boolean,
+      default: false,
+    },
+    paymentSuccess: {
+      type: Boolean,
+      default: false
+    },
+    paymentId: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+    collation: { locale: "en" },
+  }
+);
+
+OrderSchema.plugin(mongoosePaginate);
+OrderSchema.plugin(aggregatePaginate);
+OrderSchema.plugin(mongoose_delete);
+module.exports = mongoose.model("Order", OrderSchema);
